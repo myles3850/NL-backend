44c6d8ed006ff3d7c974f266e66ba1677b832671
Myles Browne <myles@browne.link>
2022-05-10 23:19:00 +0100

    reformatted index

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
 	response.json({ info: "Node.js, Express, and Postgres API", text: "pie" });
