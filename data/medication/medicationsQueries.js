745bd10d80963b51a9e09818ee1558df118b0dd4
Myles Browne <myles@browne.link>
2022-05-13 21:29:56 +0100

    added filtering and data validation

--- data/medication/medicationsQueries.js
+++ data/medication/medicationsQueries.js
@@ -1,9 +1,22 @@
-const { pool } = require('../connection');
+const { pool } = require("../connection");
 
 const getUsersMedications = (request, response) => {
-	pool.query("SELECT * FROM medication", (error, results) => {
+	//validation to see if ID field has been entered as parameter in request
+	const userId = parseInt(request.query.ID);
+	if (!request.query.ID) {
+		return response
+			.status(400)
+			.json({ error: "required field 'ID' missing, please correct and send request again" });
+	}
+	if (isNaN(userId)) {
+		return response
+			.status(400)
+			.json({ error: "required field 'ID' needs to be a number, please correct and send request again" });
+	}
+
+	pool.query("SELECT * FROM medication WHERE user = $1", [userId], (error, results) => {
 		if (error) {
-			throw error;
+			return response.status(400).send(error.detail);
 		}
 		response.status(200).json(results.rows);
 	});
@@ -19,7 +32,6 @@ const getUserById = (request, response) => {
 		response.status(200).json(results.rows);
 	});
 };
-
 
 module.exports = {
 	getUsersMedications,
