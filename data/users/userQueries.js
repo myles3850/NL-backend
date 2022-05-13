6c13005f75013eaaf1d0580e9ed6831e537d8521
Myles Browne <myles@browne.link>
2022-05-13 21:31:34 +0100

    added graceful error handling

--- data/users/userQueries.js
+++ data/users/userQueries.js
@@ -3,9 +3,9 @@ const { pool } = require('../connection');
 const getUsers = (request, response) => {
 	pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
 		if (error) {
-			throw error;
+			return response.status(409).send(error.detail);
 		}
-		response.status(200).json(results.rows);
+		return response.status(200).json(results.rows);
 	});
 };
 
@@ -14,7 +14,7 @@ const getUserById = (request, response) => {
 
 	pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
 		if (error) {
-			throw error;
+			return response.status(409).send(error.detail);
 		}
 		response.status(200).json(results.rows);
 	});
