f988c3c0bfaeea6d128eca89d7233aa2ad7e8730
myles browne <myles@browne.link>
2022-06-14 23:20:40 +0100

    changed parsing

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,16 +2,21 @@ const Pool = require("pg").Pool;
 const parse = require("pg-connection-string").parse;
 require("dotenv").config();
 
-var conn =
-	{
+var connString = process.env.DATABASE_URL;
+
+if (typeof connString === 'string') {
+	connString = parse(connString);
+} else {
+	connString = {
 		user: process.env.SQLUSER,
 		host: process.env.SQLADDR,
 		database: process.env.SQLDB,
 		password: process.env.SQLPASS,
 		port: process.env.SQLPORT,
-	} || parse(process.env.DATABASE_URL);
+	}
+}
 
-const pool = new Pool(conn);
+const pool = new Pool(connString);
 
 module.exports = {
 	pool,
