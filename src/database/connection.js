ec274fbc8b697cbea779291b8a72487483c5976e
myles browne <myles@browne.link>
2022-06-14 22:53:30 +0100

    test

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,20 +1,21 @@
 const Pool = require("pg").Pool;
-require('dotenv').config();
+require("dotenv").config();
 
-var conn = process.env.DATABASE_URL;
+var conn =
+	"postgres://tdrodwjmsnrabl:fa32a0082137861fdbc8e61e214c8a76b31b2a799a2ffb208e18509c80472d55@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/decjp0i9crljb9";
 
 if (!process.env.DATABASE_URL) {
-	conn = {	user: process.env.SQLUSER,
+	conn = {
+		user: process.env.SQLUSER,
 		host: process.env.SQLADDR,
 		database: process.env.SQLDB,
 		password: process.env.SQLPASS,
-		port: process.env.SQLPORT}
-	}
+		port: process.env.SQLPORT,
+	};
+}
 
-
-
-const pool = new Pool({'postgres://tdrodwjmsnrabl:fa32a0082137861fdbc8e61e214c8a76b31b2a799a2ffb208e18509c80472d55@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/decjp0i9crljb9'});
+const pool = new Pool(conn);
 
 module.exports = {
-  pool
+	pool,
 };
