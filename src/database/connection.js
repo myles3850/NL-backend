03ac6f00b60cddfe89f32c85428a08d7fd7a4a2f
myles browne <myles@browne.link>
2022-06-14 23:02:11 +0100

    formatting uri

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,15 @@
 const Pool = require("pg").Pool;
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 require("dotenv").config();
 
-var conn = {
-	user: process.env.SQLUSER,
-	host: process.env.SQLADDR,
-	database: process.env.SQLDB,
-	password: process.env.SQLPASS,
-	port: process.env.SQLPORT,
-} || parse(process.env.DATABASE_URL);
+var conn =
+	{
+		user: process.env.SQLUSER,
+		host: process.env.SQLADDR,
+		database: process.env.SQLDB,
+		password: process.env.SQLPASS,
+		port: process.env.SQLPORT,
+	} || parse(process.env.DATABASE_URL);
 
 const pool = new Pool(conn);
 
