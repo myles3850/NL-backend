fa316f9c00b82e751d1ed1cdd7d3e875aaa4ab63
myles browne <myles@browne.link>
2022-06-14 22:32:35 +0100

    altered database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,19 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
 
+var conn = process.env.DATABASE_URL;
 
-const pool = new Pool({
-	user: process.env.SQLUSER,
-	host: process.env.SQLADDR,
-	database: process.env.SQLDB,
-	password: process.env.SQLPASS,
-	port: process.env.SQLPORT,
-});
+if (!process.env.DATABASE_URL) {
+	conn = {	user: process.env.SQLUSER,
+		host: process.env.SQLADDR,
+		database: process.env.SQLDB,
+		password: process.env.SQLPASS,
+		port: process.env.SQLPORT}
+	}
+
+
+
+const pool = new Pool({conn});
 
 module.exports = {
   pool
