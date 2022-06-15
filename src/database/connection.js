fb95db541be907fd8b9024dbcf0cd5c5090693c9
myles browne <myles@browne.link>
2022-06-15 22:02:59 +0100

    cleaned up the mess i made

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,5 @@
 const Pool = require("pg").Pool;
-const parse = require("pg-connection-string").parse;
 require("dotenv").config();
-
-var connString = process.env.DATABASE_URL;
 
 var config = undefined;
 
@@ -24,20 +21,6 @@ if (process.env.DEV_MODE) {
 
 }
 
-
-if (typeof connString === 'string') {
-	connString = parse(connString);
-} else {
-	connString = {
-		user: process.env.SQLUSER,
-		host: process.env.SQLADDR,
-		database: process.env.SQLDB,
-		password: process.env.SQLPASS,
-		port: process.env.SQLPORT,
-	}
-}
-
-console.log(config);
 const pool = new Pool(config);
 
 module.exports = {
