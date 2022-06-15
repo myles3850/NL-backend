a3d5c5efd92501cceb57e1a54adb5c51ac7ea128
myles browne <myles@browne.link>
2022-06-15 21:53:22 +0100

    got connection string working

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,6 +4,27 @@ require("dotenv").config();
 
 var connString = process.env.DATABASE_URL;
 
+var config = undefined;
+
+if (process.env.DEV_MODE) {
+	config = {
+		user: process.env.SQLUSER,
+		host: process.env.SQLADDR,
+		database: process.env.SQLDB,
+		password: process.env.SQLPASS,
+		port: process.env.SQLPORT,
+	}
+} else {
+	config = {
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+		  rejectUnauthorized: false,
+		},
+	  }
+
+}
+
+
 if (typeof connString === 'string') {
 	connString = parse(connString);
 } else {
@@ -16,8 +37,8 @@ if (typeof connString === 'string') {
 	}
 }
 
-console.log(connString);
-const pool = new Pool({connString});
+console.log(config);
+const pool = new Pool(config);
 
 module.exports = {
 	pool,
