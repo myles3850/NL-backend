5eee513e91a64fa789be9d2f8a2fbe13677fe2b7
myles browne <myles@browne.link>
2022-06-19 15:04:51 +0100

    reformatting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const userRoutes = require("./controllers/userController");
-const medRoutes = require("./controllers/medicationController");
-const authRoutes = require("./controllers/authController");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./controllers/userController');
+const medRoutes = require('./controllers/medicationController');
+const authRoutes = require('./controllers/authController');
 
 const app = express();
 const port = process.env.APP_PORT || process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-	response.json({ info: "Node.js, Express, and Postgres API", text: "pie" });
+app.get('/', (request, response) => {
+	response.json({ info: 'Node.js, Express, and Postgres API', text: 'pie' });
 });
 
-app.use("/users", userRoutes);
-app.use("/medications", medRoutes);
-app.use("/auth", authRoutes);
+app.use('/users', userRoutes);
+app.use('/medications', medRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}.`);
