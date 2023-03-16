3c49d44393131727be4f6275e2431743db6b9eee
Choccobear <myles@browne.link>
2023-03-16 23:08:24 +0000

    added user_type to dql command

--- src/database/scripts/credentials.sql
+++ src/database/scripts/credentials.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS credentials (
    user_id INT UNIQUE NOT NULL,
    salt VARCHAR(64) NOT NULL,
    hashed_password VARCHAR(1000) NOT NULL,
-  FOREIGN KEY (user_id)
-    REFERENCES users (id)
+   user_type INT NOT NULL
+   FOREIGN KEY (user_id)
+    REFERENCES users (id),
 );
