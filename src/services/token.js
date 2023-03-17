13de6cd30c6fd0917f7904052d7365164fc99293
Choccobear <myles@browne.link>
2023-03-17 21:45:27 +0000

    capture hashed passsword from id

--- src/services/token.js
+++ src/services/token.js
@@ -3,9 +3,28 @@ const { pool } = require('../database/connection');
 const { message, httpStatusCode } = require('../utils/constants');
 
 
-const authenticateAPIRequest = (request, response) => {
+const authenticateAPIRequest = async (request, response) => {
+
+	const captureQuery = `
+	SELECT TOP 1 salt, hashed_password FROM credentials
+	WHERE user_type = 2 AND user_id IN (
+		SELECT user_id FROM users WHERE is_api_user = true 
+		AND email = $1)
+	`
+
+	const responseObj = {
+		sucsess: false,
+	}
+
 	const {ID, secret} = request.params;
 	//todo - capture the credentials and return a test token built from the credentials sent
+	const secretCredentials = await pool.query(captureQuery, [ID,]);
+	
+	if (!secretCredentials.rowCount) {
+		responseObj.message = message.INCORRECT_CREDENTIALS;
+		return response.status(httpStatusCode.UNAUTHORIZED).send(responseObj);
+	}
+	
 };
 
 module.exports = {
