73969488f620fa51483c3a24a4849ffd7f6f5d8f
choccobear <myles@browne.link>
2023-03-18 17:51:02 +0000

    moved comment to better location

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,11 +4,11 @@ const { authenticateAPIRequest } = require("../services/token");
 const { apiAuthVerification } = require("../security/apiVerification");
 
 const authRoutes = express.Router();
+// as the token endpoint needs to be avalable without verification
+//this has been locked in the controller as opposed before the controller
 
 authRoutes.post('/token', authenticateAPIRequest);
 
-// as the token endpoint needs to be avalable without verification
-//this has been locked in the controller as opposed before the controller
 authRoutes.post('/login',apiAuthVerification, authService.login);
 
 module.exports = authRoutes;
