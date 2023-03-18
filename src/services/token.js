e25471e8c9e2f44a4988650e90eb54bec3e1182b
Choccobear <myles@browne.link>
2023-03-18 01:13:04 +0000

    formatting

--- src/services/token.js
+++ src/services/token.js
@@ -3,35 +3,34 @@ const jwt = require('jsonwebtoken');
 const { pool } = require('../database/connection');
 const { message, httpStatusCode } = require('../utils/constants');
 
-
 const authenticateAPIRequest = async (request, response) => {
-	const {ID, secret} = request.body;
-	
+	const { ID, secret } = request.body;
+
 	const responseObj = {
 		sucsess: false,
-	}
+	};
 
 	const captureQuery = `
 	SELECT salt, hashed_password FROM credentials
 	WHERE user_type = 2 AND user_id IN (
 		SELECT user_id FROM users WHERE is_api_user = true 
 		AND email = $1)
-	`
+	`;
 	//todo - capture the credentials and return a test token built from the credentials sent
 	const secretCredentials = await pool.query(captureQuery, [ID]);
-	
+
 	if (!secretCredentials.rowCount) {
 		responseObj.message = message.INCORRECT_CREDENTIALS;
 		return response.status(httpStatusCode.UNAUTHORIZED).send(responseObj);
 	}
-	try{	
-		const {salt, hashed_password} = secretCredentials.rows[0];
+	try {
+		const { salt, hashed_password } = secretCredentials.rows[0];
 		const hashedPassword = hashed_password;
 		const givenPassword = await bcrypt.hash(secret, salt);
 
 		const matchingPassword = givenPassword === hashedPassword;
-		
-		if (!matchingPassword){
+
+		if (!matchingPassword) {
 			responseObj.message = message.INCORRECT_CREDENTIALS;
 			return response.status(httpStatusCode.UNAUTHORIZED).send(responseObj);
 		} else {
@@ -39,14 +38,14 @@ const authenticateAPIRequest = async (request, response) => {
 			responseObj.message = message.AUTHORISED;
 			return response.status(httpStatusCode.CREATED).send(responseObj);
 		}
-	} catch(e){
+	} catch (e) {
 		console.error(e);
-		return response(httpStatusCode.INTERNAL_SERVER_ERROR).send({message: 'internal server error'});
-		
+		return response(httpStatusCode.INTERNAL_SERVER_ERROR).send({
+			message: 'internal server error',
+		});
 	}
-
 };
 
 module.exports = {
-	authenticateAPIRequest
+	authenticateAPIRequest,
 };
