5cbdccc34482f2cfe82b06b0a9b9f47000fb566d
choccobear <myles@browne.link>
2023-03-18 16:55:50 +0000

    checking token is for a valid user

--- src/security/apiVerification.js
+++ src/security/apiVerification.js
@@ -14,17 +14,28 @@ async function apiAuthVerification(request, response, next) {
 	SELECT last_query FROM api_users 
 	WHERE token = $1`;
 
+	const updateLastQueryTime = `
+	UPDATE api_users
+	SET last_query = $1 
+	WHERE token = $2`;
+
 	try {
 	
 		const secret = process.env.JWT_SECRET;
 		await jwt.verify(token, secret)
 		const jwtPayload = jwt.decode(token)
 	
-		pool.query(getLastQueryForUser, [jwtPayload.token]);
+		const userTime = await pool.query(getLastQueryForUser, [jwtPayload.token]);
 
-		
+		if (!userTime.rowCount) {
+			//return token not recognised
+		}
+		await pool.query(updateLastQueryTime, [Date.now(), jwtPayload.token]);
+
+
+
 	} catch (e) {
-		
+		//return system error if anything falls over to here
 	}
 
 	//only call this once you have verified the token being sent is trusetd AND we have logged the access
