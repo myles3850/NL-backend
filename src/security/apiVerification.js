cf59e34fbc86cba4d660f4cd3b559695252e0c8c
Choccobear <myles@browne.link>
2023-03-18 02:51:35 +0000

    correct undefined bug

--- src/security/apiVerification.js
+++ src/security/apiVerification.js
@@ -1,12 +1,18 @@
+
 const { httpStatusCode, message } = require("../utils/constants");
+
 
 function apiAuthVerification(request, response, next) {
 	const token = request.get('Authorization');
 
-	if (token.isUndefined()) {
+	if (token === undefined) {
 		return response.status(httpStatusCode.UNAUTHORIZED).send(message.INCORRECT_CREDENTIALS);
 	}
 
 	//only call this once you have verified the token being sent is trusetd AND we have logged the access
 	next();
+}
+
+module.exports = {
+	apiAuthVerification,
 }
